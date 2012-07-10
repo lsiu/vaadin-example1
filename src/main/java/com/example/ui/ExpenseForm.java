a507cc7f0c5c808a4f507569be859ef0ccdc6e3b
lsiu <leonard.siu@gmail.com>
2012-07-11 00:11:34 +0800

    Add save button to expense form.

--- src/main/java/com/example/ui/ExpenseForm.java
+++ src/main/java/com/example/ui/ExpenseForm.java
@@ -2,16 +2,23 @@ package com.example.ui;
 
 import com.example.model.Expense;
 import com.vaadin.data.util.BeanItem;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.CustomComponent;
 import com.vaadin.ui.Form;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Layout;
 import com.vaadin.ui.VerticalLayout;
 
 @SuppressWarnings("serial")
-public class ExpenseForm extends CustomComponent {
+public class ExpenseForm extends CustomComponent implements ClickListener {
 	
-	private Layout mainLayout;
+	private VerticalLayout mainLayout;
 	private Form form;
+	private Button saveButton = new Button("Save", this);
+	private Expense expense = new Expense();
 
 	public ExpenseForm() {
 		initMainLayout();
@@ -19,12 +26,32 @@ public class ExpenseForm extends CustomComponent {
 		
 		form = buildForm();
 		mainLayout.addComponent(form);
+		
+		Layout buttonPane = buildButtonPane();
+		mainLayout.addComponent(buttonPane);
+		mainLayout.setComponentAlignment(buttonPane, Alignment.MIDDLE_CENTER);
+	}
+	
+	public void buttonClick(ClickEvent event) {
+		if (event.getButton() == saveButton) {
+			String msg = String.format("Date: %s, Summary: %s, Amount: %s", 
+					expense.getTransactionDate(),
+					expense.getSummary(),
+					expense.getAmount());
+			getWindow().showNotification("Expense Saved", msg);
+		}
+	}
+
+	private Layout buildButtonPane() {
+		HorizontalLayout pane = new HorizontalLayout();
+		pane.addComponent(saveButton);
+		return pane;
 	}
 
 	private Form buildForm() {
 		form = new Form();
 		form.setFormFieldFactory(new ExpenseFieldFactory());
-		form.setItemDataSource(new BeanItem<Expense>(new Expense()));
+		form.setItemDataSource(new BeanItem<Expense>(expense));
 		form.setVisibleItemProperties(new Object[] { "transactionDate", "summary", "amount"});
 		return form;
 	}
