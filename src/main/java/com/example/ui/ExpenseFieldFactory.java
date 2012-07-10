8cc017fbf898b2abeae260d6d06c89f6d46f0f40
lsiu <leonard.siu@gmail.com>
2012-07-10 23:57:16 +0800

    Use TextArea for summary field

--- src/main/java/com/example/ui/ExpenseFieldFactory.java
+++ src/main/java/com/example/ui/ExpenseFieldFactory.java
@@ -4,6 +4,7 @@ import com.vaadin.data.Item;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.DefaultFieldFactory;
 import com.vaadin.ui.Field;
+import com.vaadin.ui.TextArea;
 import com.vaadin.ui.TextField;
 
 @SuppressWarnings("serial")
@@ -11,6 +12,13 @@ public class ExpenseFieldFactory extends DefaultFieldFactory {
 
 	@Override
 	public Field createField(Item item, Object propertyId, Component uiContext) {
+		if ("summary".equals(propertyId)) {
+			TextArea field = new TextArea("Summary");
+			field.setPropertyDataSource(item.getItemProperty(propertyId));
+			field.setNullRepresentation("");
+			field.setWidth("100%");
+			return field;
+		}
 		Field field = super.createField(item, propertyId, uiContext);
 		if (field instanceof TextField)
 			((TextField) field).setNullRepresentation("");
