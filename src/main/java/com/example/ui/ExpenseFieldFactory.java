e7bbfc90c152af97178fc463dcc0ad022d2a744d
lsiu <leonard.siu@gmail.com>
2012-07-11 00:28:28 +0800

    Add required validation to amount and transaction date field. Also add "DoubleValidator" to amount field.

--- src/main/java/com/example/ui/ExpenseFieldFactory.java
+++ src/main/java/com/example/ui/ExpenseFieldFactory.java
@@ -1,6 +1,7 @@
 package com.example.ui;
 
 import com.vaadin.data.Item;
+import com.vaadin.data.validator.DoubleValidator;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.DefaultFieldFactory;
 import com.vaadin.ui.Field;
@@ -22,6 +23,14 @@ public class ExpenseFieldFactory extends DefaultFieldFactory {
 			return field;
 		}
 		Field field = super.createField(item, propertyId, uiContext);
+		if ("amount".equals(propertyId)) {
+			field.setRequired(true);
+			field.setRequiredError("A valid expense amount is required");
+			field.addValidator(new DoubleValidator("Invalid value for Amount field"));
+		} else if ("transactionDate".equals(propertyId)) {
+			field.setRequired(true);
+			field.setRequiredError("A valid transaction date for the expense is required");
+		}
 		if (field instanceof TextField)
 			((TextField) field).setNullRepresentation("");
 		return field;
